3f428807c8c5ffc8b09c70fc12b3080fba728b0c
Emmanuel Popa <emmanuelaaronpopa@gmail.com>
2025-07-02 13:09:46 -0400

    feat: vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import localFont from "next/font/local";
 import { ThemeProvider } from "@/components/theme-provider";
 import { MainNav } from "@/components/main-nav";
 import { Footer } from "@/components/footer";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -43,6 +44,7 @@ export default function RootLayout({
           <MainNav />
           {children}
           <Footer />
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
